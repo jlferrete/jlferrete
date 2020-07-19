ddeccafc3643f78f7533d9597e73d2538c5186bb
jlferrete <joseluisferrete@gmail.com>
2020-07-19 10:49:09 +0200

    Autoupdated articles from blog

---
+++ index.js
@@ -0,0 +1,26 @@
+const fs = require('fs').promises
+const Parser = require('rss-parser')
+const parser = new Parser()
+
+const LASTEST_ARTICLE_PLACEHOLDER = /%{{latest_article}}%/g
+const ICONS_SIZE_PLACEHOLDER = /%{{icon_size}}%/g
+const ICONS_SIZE = '24px'
+
+    ; (async () => {
+        const markdownTemplate = await fs.readFile('./README.md.tpl', { encoding: 'utf-8' })
+        const items = await parser.parseURL('https://jlferrete.com/feed/');
+        // put the latest article
+        const [{ title, link }] = items.items
+        const latestArticleMarkdown = `[${title}](${link})`
+
+        //replace all placeholders with info
+        const newMarkdown = markdownTemplate
+            .replace(LASTEST_ARTICLE_PLACEHOLDER, latestArticleMarkdown)
+            .replace(ICONS_SIZE_PLACEHOLDER, ICONS_SIZE) //put the icon size to all icons
+
+        await fs.writeFile('./README.md', newMarkdown)
+
+
+    })()
+
+
